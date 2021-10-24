10abec5e62a930e83053157a250098e700552923
Carl-Jin <buffpal2016@gmail.com>
2021-10-24 09:50:33 -0400

    docs: 添加 ie 11 兼容方案

--- static/loadPolyfill.js
+++ static/loadPolyfill.js
@@ -1,10 +1,25 @@
+"use strict";
+
 (function () {
   var u = navigator.userAgent;
+
   if (/MSIE \d|Trident.*rv:/.test(u)) {
     var polyfillScript = document.createElement("script");
     polyfillScript.type = "text/javaScript";
     polyfillScript.src =
       "https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.12.1/polyfill.min.js";
     document.getElementsByTagName("head")[0].appendChild(polyfillScript);
+
+    window.addEventListener("DOMContentLoaded", function () {
+      var links = document.querySelectorAll("link");
+
+      for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+
+        if (~link.href.indexOf("/assets/css/style")) {
+          link.href = link.href.replace(/\.css$/, ".ie11.css");
+        }
+      }
+    });
   }
 })();
